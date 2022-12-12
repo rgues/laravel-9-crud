812defeb909cc3e433fc751629b42dab81e1aafb
kamen ntchabet <rgueskamen@gmail.com>
2022-12-13 01:00:57 +0400

    CRUD for product

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,55 @@
+@extends('products.layout')
+ 
+@section('content')
+<div class="row">
+   <div class="col-lg-12 margin-tb">
+      <div class="pull-left">
+         <h2>Laravel 9 CRUD Operations</h2>
+      </div>
+      <div class="pull-right">
+         <a class="btn btn-success" href="{{ route('products.create') }}">Create New Product</a>
+      </div>
+   </div>
+</div>
+ 
+@if ($message = Session::get('success'))
+<div class="alert alert-success">
+   <p>{{ $message }}</p>
+</div>
+@endif
+ 
+<table class="table table-bordered">
+    <thead>
+       <tr>
+          <th>No.</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Description</th>
+          <th width="280px">Action</th>
+       </tr>
+    </thead>
+    
+   <tbody>
+       @foreach ($products as $product)
+           <tr>
+              <td>{{ ++$i }}</td>
+              <td>{{ $product->name }}</td>
+              <td>{{ $product->price }}</td>
+              <td>{{ $product->description }}</td>
+              <td>
+                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
+                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+                    <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+           </tr>
+       @endforeach
+    </tbody>
+</table>
+ 
+{!! $products->links() !!}
+ 
+@endsection
